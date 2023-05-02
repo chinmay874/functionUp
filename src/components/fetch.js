a3e1556dd544312d810c4bf32e611c79d05a280a
chinmay jain <chinmay874@gmail.com>
2023-05-02 23:37:42 +0530

    complete fetch api

---
+++ src/components/fetch.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react"
+
+
+  export function Fetchapi(){
+    const [img, setimg]=useState()
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response)=>response.json())
+        .then((data)=>setimg(data.message))
+        console.log({img});
+    },[])
+    
+        return(
+            <img src={img} alt="" style={{height:"250px", width:"200px"}}/>
+
+        )
+    }
