e0f8281b070ed1713a95e9253d3f63ffdd67500c
chinmay jain <chinmay874@gmail.com>
2023-05-05 09:59:17 +0530

    changes

--- src/components/fetch.js
+++ src/components/fetch.js
@@ -1,14 +1,30 @@
 import { useEffect, useState } from "react"
+import axios from "axios";
 
 
   export function Fetchapi(){
     const [img, setimg]=useState()
+
+    // async function waitapi(){
+    //     const ruko= await fetch("https://dog.ceo/api/breeds/image/random")
+    //     const data= await ruko.json()
+    //     setimg(data.message)
+    // }
+    
+    
+    
     useEffect(()=>{
-        fetch("https://dog.ceo/api/breeds/image/random")
-        .then((response)=>response.json())
-        .then((data)=>setimg(data.message))
-        console.log({img});
+       // waitapi()
+       axios.get("https://dog.ceo/api/breeds/image/random")
+       .then((response)=>setimg(response.data.message))
     },[])
+
+
+    // useEffect(()=>{
+    //      fetch("https://dog.ceo/api/breeds/image/random")
+    //     .then((response)=>response.json())
+    //     .then((data)=>setimg(data.message))
+    // },[])
     
         return(
             <img src={img} alt="" style={{height:"250px", width:"200px"}}/>
