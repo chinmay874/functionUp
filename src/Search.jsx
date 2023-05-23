5c894d4e36ff392320b11b3528f73b3888b9baee
chinmay jain <chinmay874@gmail.com>
2023-05-23 10:04:16 +0530

    submit

---
+++ src/Search.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import './Search.css';
+export function Search(){
+    const[data,setData]=useState([]);
+    const [filterData,setFilterData]=useState([]);
+
+    useEffect(()=>{
+        fetch('http://cdn-api.co-vin.in/api/v2/admin/location/states')
+        .then(res => res.json())
+        .then(res=> {
+            console.log(res);
+            setFilterData(res.states);
+        })
+        .catch(err=> console.log(err));
+    },[])
+
+    
+    const handleFilter=(value)=>{
+        const response= filterData.filter(f => (f.state_name.toLowerCase().includes(value)))
+        setData(response);
+        console.log(data)
+    }
+    return(
+    <div className='Search-container'>
+        <div className="search">
+            <input type="text" placeholder="Search here..." onChange={e =>handleFilter(e.target.value)}/>
+        </div>
+        <div className='search-result'>
+            {
+            data.map((d,i)=>( 
+                <div key={i}>{d.state_name}</div>
+            ))}
+        </div>
+    </div>
+    )
+}
